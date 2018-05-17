ab04d48f3bc402b055b25f52e59c79e57c3819b5
Yuanhui Wang <yuanhuiw@vmware.com>
2018-05-17 15:51:04 +0800

    test order

--- bean-lifecycle/src/main/java/BeanLifecycle/processor/TestPostProcessorFirst.java
+++ bean-lifecycle/src/main/java/BeanLifecycle/processor/TestPostProcessorFirst.java
@@ -2,10 +2,11 @@ package BeanLifecycle.processor;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TestPostProcessorFirst implements BeanPostProcessor {
+public class TestPostProcessorFirst implements BeanPostProcessor, Ordered{
     public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
         System.out.println("First.ProcessBeforeInitialization: " + s);
         return o;
@@ -14,5 +15,9 @@ public class TestPostProcessorFirst implements BeanPostProcessor {
     public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
         System.out.println("First.ProcessAfterInitialization: " + s);
         return o;
+    }
+
+    public int getOrder() {
+        return 3;
     }
 }
--- bean-lifecycle/src/main/java/BeanLifecycle/processor/TestPostProcessorSecond.java
+++ bean-lifecycle/src/main/java/BeanLifecycle/processor/TestPostProcessorSecond.java
@@ -2,10 +2,11 @@ package BeanLifecycle.processor;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TestPostProcessorSecond implements BeanPostProcessor{
+public class TestPostProcessorSecond implements BeanPostProcessor, Ordered{
     public Object postProcessBeforeInitialization(Object o, String s) throws BeansException {
         System.out.println("Second.ProcessBeforeInitialization: " + s);
         return o;
@@ -14,5 +15,9 @@ public class TestPostProcessorSecond implements BeanPostProcessor{
     public Object postProcessAfterInitialization(Object o, String s) throws BeansException {
         System.out.println("Second.ProcessAfterInitialization: " + s);
         return o;
+    }
+
+    public int getOrder() {
+        return 2;
     }
 }
